59bdae3f4a6127ba652509673456caf48d8ef5bf
Agness David <Agnessdavi12@gmail.com>
2025-09-25 18:24:13 +0000

    Workspace

--- Projet1/app/Filament/Resources/Clients/Schemas/ClientForm.php
+++ Projet1/app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -2,6 +2,63 @@
 
 namespace App\Filament\Resources\Clients\Schemas;
 
+
+use Filament\Forms\Components\Card;
+use Filament\Schemas\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ClientForm
+{
+  public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Informations personnelles')
+                    ->schema([
+                         TextInput::make('nom')
+                    ->required(),
+                Select::make('type')
+                    ->options(['societe' => 'Societe', 'organisme' => 'Organisme', 'particulier' => 'Particulier'])
+                    ->default('societe')
+                    ->required(),
+                                     TextInput::make('cellulaire'),
+                TextInput::make('fax'),
+                TextInput::make('email')
+                    ->label('Email address'),
+                     TextInput::make('fonction')
+                    ])
+                    ->columns(2),
+
+                Section::make('Profil professionnel')
+                    ->schema([
+             TextInput::make('ville'),
+                TextInput::make('quartier_de_residence'),
+                Select::make('usage')
+                    ->options(['personnel' => 'Personnel', 'professionnel' => 'Professionnel']),
+                TextInput::make('domaine_activite'),
+                    ])
+                    ->columns(2),
+
+               /* Section::make('Médias / Portfolio')
+                    ->schema([
+                        
+                    ])
+                    ->columns(2),*/
+            ]);
+    }
+}
+
+
+/*
+
+<?php
+
+namespace App\Filament\Resources\Clients\Schemas;
+
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
@@ -34,4 +91,4 @@ class ClientForm
                 TextInput::make('domaine_activite'),
             ]);
     }
-}
+} */
